5a20605c3f6505ea3c44ccda039b87240f34eca7
Greg Larkin <greg.larkin@puppetlabs.com>
2015-06-02 10:45:08 -0500

    Update r10k_syntax_check.sh

--- commit_hooks/r10k_syntax_check.sh
+++ commit_hooks/r10k_syntax_check.sh
@@ -2,6 +2,9 @@
 #
 # This script assumes you have installed r10k and will perform a syntax check on the Puppetfile if existing
 
+# Disable this check for now - r10k.yaml is managed by the r10k module
+exit 0
+
 echo "Performing a syntax check on the r10k Puppetfile:"
 r10k puppetfile check
 
